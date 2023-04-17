0f9c27a1c31bf06c4b8dd50780b62c54633e9ecb
flaviosoliver <flavsoliver@gmail.com>
2023-04-17 19:16:14 -0300

    add components app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,35 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MaterialModule } from './app-material.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './app-material.module';
+import { NgxMatIntlTelInputComponent } from 'ngx-mat-intl-tel-input';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { CheckNumberComponent } from './components/check-number/check-number.component';
+import { SelectPlatformComponent } from './components/select-platform/select-platform.component';
+import { FormNumberComponent } from './components/form-number/form-number.component';
+import { SendToComponent } from './components/send-to/send-to.component';
 
 @NgModule({
-  declarations: [AppComponent, ToolbarComponent],
+  declarations: [
+    AppComponent,
+    ToolbarComponent,
+    CheckNumberComponent,
+    SelectPlatformComponent,
+    FormNumberComponent,
+    SendToComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
     MaterialModule,
+    NgxMatIntlTelInputComponent,
   ],
   providers: [],
   bootstrap: [AppComponent],
